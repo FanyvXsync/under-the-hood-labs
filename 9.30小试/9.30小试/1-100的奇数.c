84d9977042df7989b1622192626d6ac2d2708a8b
gaifanaas <si428402273@163.com>
2026-01-15 16:03:35 +0800

    9.30

---
+++ 9.30小试/9.30小试/1-100的奇数.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int a = 0;
+
+	while (i < 101);
+	{
+		i = i++;
+		a = i;
+		if (a % 2 == 1);
+		printf("%d\n",a);
+	}
+	return 0;
+
+
+
+
+}
